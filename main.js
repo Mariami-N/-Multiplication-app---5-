debba6e7df99e3b07c2f58ea4a2c91f41826e013
Mariami-N <avetianimari@gmail.com>
2023-02-28 00:03:44 -0800

    multiplication app__5

--- main.js
+++ main.js
@@ -5,31 +5,28 @@ let h1=document.querySelector("h1");
 let input=document.querySelector("input");
 let button=document.querySelector("button");
 
-let number=0;
-    score.innerHTML=`score: ${number}`;
 
 let num1=0; let num2=0;
 num1=Math.floor(Math.random()* 10);
 num2=Math.floor(Math.random()* 10);
 h1.innerHTML=`what  is  ${num1}  multiply  by  ${num2}?`;
 
-
-let scores = JSON.parse(localStorage.getItem("scores")) || [];
-number=0;
+button.disabled = false;
+let scores = JSON.parse(localStorage.getItem("scores")) || 0;
+score.innerHTML=`score: ${scores}`;
 button.addEventListener("click", function (){
-    if (input.value == num1 * num2){
-        number++
-    }else if (input.value !== num1 * num2){
-        number--
+    location.reload()
+    console.log(input.value)
+   if(input.value == ""){
+    button.disabled = true
+   }else if ( input.value == num1 * num2){
+        scores++
+    }else if ( input.value !== num1 * num2){
+        scores--
     }
-    score.innerHTML=`score: ${number}`;
-    scores.push(number);
+    score.innerHTML=`score: ${scores}`;
     localStorage.setItem("scores", JSON.stringify(scores));
     input.value = "";
 });
 
-scores.forEach((number2) => {
-    score.innerHTML =`score: ${number2}`;
-    number2++
-    
-});
+
